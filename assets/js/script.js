c8d03c7bf2415d69d5e5a49ad50d72d6ab18553b
MJB <matt.berti13@gmail.com>
2022-03-03 14:56:26 -0600

    made trash widget functional and added touch punch script

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,6 +206,21 @@ $(".card .list-group").sortable({
    saveTasks();
   }
 });
+// Make trash a droppable to remove individual tasks
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event,ui){
+    console.log("out");
+  }
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
