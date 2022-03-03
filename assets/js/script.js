7c74cda2f3ace5cee08fc4b6f4624e183a899cc8
MJB <matt.berti13@gmail.com>
2022-03-03 14:37:31 -0600

    added drag and drop ability between lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,6 +156,56 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+//Drag and Drop
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  deactivate: function(event){
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", this);
+  },
+  out: function(event){
+    console.log("out", this);
+  },
+  update: function(event){
+    //create array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text= $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date= $(this)
+      .find("span")
+      .text()
+      .trim();
+
+     //add task data to the temp array as an object
+     tempArr.push({
+       text: text,
+       date: date
+     });
+    });
+   //trim down the list's ID to match object property
+   var arrName = $(this)
+   .attr("id")
+   .replace("list-", "");
+
+   //update array on tasks object and save
+   tasks[arrName] =tempArr;
+   saveTasks();
+  }
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
